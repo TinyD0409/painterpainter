286a3ee2c13605dd7eedf21ec9cb26699a1a5477
TinyD0409 <ningkid345@163.com>
2018-11-14 23:17:21 +0800

    缩放时以鼠标为原点

--- readme.md
+++ readme.md
@@ -1,10 +1,35 @@
 # Pixel Painter
 
-# Tech used
+### 主要功能
 
-* canvas
-* websocket
-* express
-* socket.io
-* Buffer
-* pixel manipulation
+* 使用canvas绘制，以节省内存提升性能
+* 使用websocket进行实时传输
+* 使用express构建http服务器
+* 使用ws模块构建websocket后端并且与express集成
+* 后端使用buffer保存图片实时数据
+* 后端自动保存图片状态，服务器重启也不会丢失
+* 首次打开页面时，后端将图片转为png编码传给前端，以节省浏览，加快打开速度
+* 取色功能
+* 取色实时提示
+* 缩放
+* 移动
+
+### 应用技术
+
+#### 服务端方面
+
+express、node 搭建后端服务器
+利用arraybuffer 转换为png图片节省流量
+websocket和socket.io进行信息传输
+以及各种包的应用
+
+#### 客户端（前端）方面
+
+整体使用Vue框架开发
+利用 canvas 在网页端绘制，节省性能，客户端更流畅
+
+### 技术难点
+
+鼠标点击位置的精确
+
+拖拽功能
--- src/app.js
+++ src/app.js
@@ -1,16 +1,13 @@
 const http = require('http')
-const fs = require('fs')
 const path = require('path')
 
-const ws = require('ws')
+const socketIO = require('socket.io')
 const express = require('express')
 const Jimp = require('jimp')
 const port = 9095
 const app = express() //为了使用express中间件 ，而这些中间件转为了这个函数
 const server = http.createServer(app) 
-
-const wss = new ws.Server({server})
-
+const io = socketIO(server)
 const width = 1280
 const height = 720
 
@@ -36,15 +33,22 @@ async function main(){
     }
   },3000)
   
-  
-  wss.on('connection',(ws,req) => {//req上有很多信息，有利于以后做账号系统
+  global.io = io
+  let userOperations = []
+  setInterval(() => {
+    if (userOperations.length){
+      io.emit('updateDot', userOperations)
+      userOperations = []
+    }
+  }, 300)
+  io.on('connection',(ws,req) => {//req上有很多信息，有利于以后做账号系统
     //监听了这个连接
     //先send 这个画布的pixelData 信息
-    img.getBuffer(Jimp.MIME_PNG,(err,buf) => {
+    img.getBuffer(Jimp.MIME_PNG,(err, buf) => {
       if(err){
         console.log('get buffer err',err)
-      }else {
-        ws.send(buf)
+      } else {
+        ws.emit('init', buf)
       }
     })
     //ws.send(JSON.stringify({ 
@@ -55,50 +59,70 @@ async function main(){
     //}))
     
     //有人进来发送一个json,而且每来一个人都要广播一下,而不是给一个人发
-    wss.clients.forEach(ws => {
-      ws.send(JSON.stringify({
-        type: 'onlineCount',
-        count: wss.clients.size,
-      }))
+    // wss.clients.forEach(ws => {
+    //   ws.send(JSON.stringify({
+    //     type: 'onlineCount',
+    //     count: wss.clients.size,
+    //   }))
+    // })
+    //用socketio更方便 （类似react里事件的派发
+    io.emit('onlineCount',{
+      count: Object.keys(io.sockets.sockets).length
     })
+
     //有人走 也发送一个json
     ws.on('close', () => {
-      wss.clients.forEach(ws => {
-        ws.send(JSON.stringify({
-          type: 'onlineCount',
-          count: wss.clients.size,
-        }))
+      io.emit('onlineCount', {
+        count: Object.keys(io.sockets.sockets).length
       })
     })
     var lastDraw = 0
-  
-    //监听 点的哪个点被点 ，想所有用户 发送回去
-    ws.on('message',msg => {  //node环境下 都是on('事件')往上挂 而浏览器环境下就是 on事件名 或addeventlistener
-      msg = JSON.parse(msg)
-  
+    
+    ws.on('drawDot', data => {
       var now = Date.now()
-      var {x,y,color} = msg
-      if(msg.type == 'drawdot'){ 
-        if(now - lastDraw < 200){ //建立连接的用户才能按这个频率来发
-          return
-        }
-        if(x > 0 && y > 0 && x < width && y < height){ //限制宽高
-          lastDraw = now
-          lastUpdate = now
-          // pixelData[msg.y][msg.x] = msg.color
-          img.setPixelColor(Jimp.cssColorToHex(color),x,y) 
-          wss.clients.forEach(client => { 
-            //clients 方法：A set that stores all connected clients. 
-            //Please note that this property is only added when the clientTracking is truthy.
-            //收到任何一个客户端发出的请求，发送给每个客户端
-            client.send(JSON.stringify({
-              type: 'updatedot',
-              x,y,color
-            }))
-          })
-        }
+      var {x,y,color} = data
+      if(now - lastDraw < 200){ //建立连接的用户才能按这个频率来发
+        return
+      }
+      if(x > 0 && y > 0 && x < width && y < height){ //限制宽高
+        lastDraw = now
+        lastUpdate = now
+        // pixelData[msg.y][msg.x] = msg.color
+        img.setPixelColor(Jimp.cssColorToHex(color),x,y)
+        // io.emit('updateDot', {
+        //   x,y,color
+        // })
+        userOperations.push({x,y,color})
+        //console.log(userOperations)
       }
     })
+    //监听 点的哪个点被点 ，想所有用户 发送回去
+    // ws.on('message',msg => {  //node环境下 都是on('事件')往上挂 而浏览器环境下就是 on事件名 或addeventlistener
+    //   msg = JSON.parse(msg)
+  
+    //   var now = Date.now()
+    //   var {x,y,color} = msg
+    //   if(msg.type == 'drawDot'){ 
+    //     if(now - lastDraw < 200){ //建立连接的用户才能按这个频率来发
+    //       return
+    //     }
+    //     if(x > 0 && y > 0 && x < width && y < height){ //限制宽高
+    //       lastDraw = now
+    //       lastUpdate = now
+    //       // pixelData[msg.y][msg.x] = msg.color
+    //       img.setPixelColor(Jimp.cssColorToHex(color),x,y) 
+    //       wss.clients.forEach(client => { 
+    //         //clients 方法：A set that stores all connected clients. 
+    //         //Please note that this property is only added when the clientTracking is truthy.
+    //         //收到任何一个客户端发出的请求，发送给每个客户端
+    //         client.send(JSON.stringify({
+    //           type: 'updateDot',
+    //           x,y,color
+    //         }))
+    //       })
+    //     }
+    //   }
+    // })
   })
   app.use(express.static(path.join(__dirname,'./static')))
   
